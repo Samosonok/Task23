ae51a93f21a2979d798e711ea2cc39b0cec8bf4b
Liliya Samosonok <samosonochek@gmail.com>
2021-10-31 03:46:25 +0300

    more code correction

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -8,11 +8,13 @@ public class Book {
 
     @Pseudonym("book name")
     private String title;
+    private String writer;
     @Ignore
     private int pages;
 
-    public Book(String title, int pages) {
+    public Book(String title, String writer, int pages) {
         this.title = title;
+        this.writer = writer;
         this.pages = pages;
     }
 }
--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -7,39 +7,40 @@ import java.lang.reflect.Field;
 
 public class Converter {
 
-    static Book book = new Book("Witcher", 600);
+    static Book book = new Book("Witcher", "Sapkowski", 600);
     static String result;
+    static String tagName1;
+    static String tagName2;
+    static Object fieldName1;
+    static Object fieldName2;
 
-    public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
-        System.out.println(ConvertBook(book));
+    public static void main(String[] args) throws IllegalAccessException {
+        System.out.println(convertSomeObject(book));
     }
 
-    public static String ConvertBook(Object object) throws IllegalAccessException, NoSuchFieldException {
+    public static String convertSomeObject(Object object) throws IllegalAccessException {
         Class<?> clazz = object.getClass();
         String className = clazz.getSimpleName().toLowerCase();
         Field[] fields = clazz.getDeclaredFields();
 
-        String field1 = getAnnotationValue(fields);
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.isAnnotationPresent(Ignore.class)) {
+                field.getDeclaredAnnotation(Ignore.class);
+            } else if (field.isAnnotationPresent(Pseudonym.class)) {
+                tagName1 = field.getDeclaredAnnotation(Pseudonym.class).value();
+                fieldName1 = field.get(object);
+            } else {
+                tagName2 = field.getName();
+                fieldName2 = field.get(object);
+            }
+        }
 
-        Field titleField = clazz.getDeclaredField("title");
-
-        titleField.setAccessible(true);
-
-        Object title = titleField.get(object);
-
-        result = ("<" + className + ">\n" +
-                "   <" + field1 + ">" + title + "</" + field1 + ">\n" +
-                "</" + className + ">\n");
+        result = ("<" + className + ">" +
+                "\n   <" + tagName1 + ">" + fieldName1 + "</" + tagName1 + ">" +
+                "\n   <" + tagName2 + ">" + fieldName2 + "</" + tagName2 + ">" +
+                "\n</" + className + ">");
 
         return result;
-    }
-
-    public static String getAnnotationValue(Field[] fields){
-        for (Field field : fields) {
-            if (field.isAnnotationPresent(Pseudonym.class)) {
-                return field.getDeclaredAnnotation(Pseudonym.class).value();
-            } else return null;
-        }
-        return null;
     }
 }
